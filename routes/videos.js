836f57b3ae990d03a159435adf4f5b6343585ddd
Aelitta Ezugbaya <aelittae@gmail.com>
2017-08-22 14:25:41 +0300

    tryed to replace monk to mongoose

--- routes/videos.js
+++ routes/videos.js
@@ -1,11 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
-var monk = require('monk');
-var db = monk('localhost:27017/vidzy');
+var mongoose = require( 'mongoose' );
 
+var db = 'mongodb://localhost:27017/vydzy';
+mongoose.connect(db,{useMongoClient: true});
+
+mongoose.connection.on('connected', function () {
+ console.log('Mongoose connected to ' + db);
+});
+mongoose.connection.on('error',function (err) {
+ console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+ console.log('Mongoose disconnected');
+});
+
+var gracefulShutdown = function (msg, callback) {
+ mongoose.connection.close(function () {
+ console.log('Mongoose disconnected through ' + msg);
+ callback();
+ });
+}
+
+process.once('SIGUSR2', function () {
+ gracefulShutdown('nodemon restart', function () {
+ process.kill(process.pid, 'SIGUSR2');
+ });
+});
+
+process.on('SIGINT', function () {
+ gracefulShutdown('app termination', function () {
+ process.exit(0);
+ });
+});
+
+
+// var monk = require('monk');
+// var db = monk('localhost:27017/vidzy');
+//
 router.get('/', function(req, res) {
     var collection = db.get('videos');
+    console.log(db.getCollection('videos').find({}));
     collection.find({}, function(err, videos){
         if (err) throw err;
       	res.json(videos);
